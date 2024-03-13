1c6af504301a130d11d67598e0cd7c8934d6997f
gustjdw <johs710@gachon.ac.kr>
2024-03-13 13:46:25 +0900

    Initial commit

---
+++ src/main/java/com/geus/GeusApplication.java
@@ -0,0 +1,13 @@
+package com.geus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geus/GeusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
